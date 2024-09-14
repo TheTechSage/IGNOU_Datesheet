ba8b5a541b1e1d7f1c272e864830daaab1d95da1
The Tech Sage <103244076+TheTechSage@users.noreply.github.com>
2024-09-14 23:02:32 +0530

    added data to datesheet, corrected links & layout structure of the app

---
+++ src/components/Header.tsx
@@ -0,0 +1,122 @@
+import { useState } from "react";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
+
+const navigation = [
+    { name: "Home", href: "/" },
+    { name: "Datesheet", href: "/datesheet" },
+    // { name: "Product", href: "#" },
+    // { name: "Features", href: "#" },
+    // { name: "Marketplace", href: "#" },
+    // { name: "Company", href: "#" },
+];
+
+export default function Header() {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+    
+    return (
+        <div className="sticky top-0 z-50 bg-white/70 bg-blend-color-dodge">
+            <header>
+                <nav
+                    aria-label="Global"
+                    className="flex items-center justify-between p-3 lg:px-8"
+                >
+                    <div className="flex lg:mr-10 ">
+                        <Link to="/" className="-m-1.5 p-1.5">
+                            <span className="sr-only">Logo</span>
+                            <img
+                                alt="logo"
+                                src="src/assets/ignou_logo.svg"
+                                className="w-auto h-8"
+                            />
+                        </Link>
+                    </div>
+                    <div className="flex lg:hidden">
+                        <button
+                            type="button"
+                            onClick={() => setMobileMenuOpen(true)}
+                            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 "
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            <Bars3Icon aria-hidden="true" className="w-6 h-6" />
+                        </button>
+                    </div>
+                    <div className="justify-center flex-1 hidden lg:flex lg:gap-x-40 min-h-[28px]">
+                        {navigation.map((item) => (
+                            <Link
+                                key={item.name}
+                                to={item.href}
+                                className="text-base font-semibold leading-6 text-gray-900 hover:border-b-4 hover:border-sky-500 active:border-sky-500 active:border-b-2"
+                            >
+                                {item.name}
+                            </Link>
+                        ))}
+                    </div>
+                    {/* <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                        <a
+                            href="#"
+                            className="text-sm font-semibold leading-6 text-gray-900"
+                        >
+                            Log in <span aria-hidden="true">&rarr;</span>
+                        </a>
+                    </div> */}
+                </nav>
+
+                <Dialog
+                    open={mobileMenuOpen}
+                    onClose={setMobileMenuOpen}
+                    className="lg:hidden"
+                >
+                    <div className="fixed inset-0 z-50" />
+                    <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full px-6 py-6 overflow-y-auto bg-white sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                        <div className="flex items-center justify-between">
+                            <Link to="/" className="-m-1.5 p-1.5">
+                                <span className="sr-only">Logo</span>
+                                <img
+                                    alt="logo"
+                                    src="src/assets/ignou_logo.svg"
+                                    className="w-auto h-8"
+                                />
+                            </Link>
+                            <button
+                                type="button"
+                                onClick={() => setMobileMenuOpen(false)}
+                                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                            >
+                                <span className="sr-only">Close menu</span>
+                                <XMarkIcon
+                                    aria-hidden="true"
+                                    className="w-6 h-6"
+                                />
+                            </button>
+                        </div>
+                        <div className="flow-root mt-6">
+                            <div className="-my-6 divide-y divide-gray-500/10">
+                                <div className="py-6 space-y-2">
+                                    {navigation.map((item) => (
+                                        <Link
+                                            key={item.name}
+                                            to={item.href}
+                                            className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-sky-500 active:bg-sky-500"
+                                        >
+                                            {item.name}
+                                        </Link>
+                                    ))}
+                                </div>
+                                {/* <div className="py-6">
+                                    <a
+                                        href="#"
+                                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                    >
+                                        Log in
+                                    </a>
+                                </div> */}
+                            </div>
+                        </div>
+                    </DialogPanel>
+                </Dialog>
+            </header>
+        </div>
+    );
+}
--- src/pages/Datesheet.tsx
+++ src/pages/Datesheet.tsx
@@ -3,85 +3,176 @@ import {
     TableBody,
     TableCaption,
     TableCell,
-    TableFooter,
     TableHead,
     TableHeader,
     TableRow,
-  } from "@/components/ui/table"
+} from "@/components/ui/table";
+import {
+    courses_1,
+    courses_2,
+    courses_3,
+    courses_4,
+    Course,
+} from "../components/course.ts";
+import { FunctionComponent as FC } from "react";
 
-  const invoices = [
-    {
-      invoice: "INV001",
-      paymentStatus: "Paid",
-      totalAmount: "$250.00",
-      paymentMethod: "Credit Card",
-    },
-    {
-      invoice: "INV002",
-      paymentStatus: "Pending",
-      totalAmount: "$150.00",
-      paymentMethod: "PayPal",
-    },
-    {
-      invoice: "INV003",
-      paymentStatus: "Unpaid",
-      totalAmount: "$350.00",
-      paymentMethod: "Bank Transfer",
-    },
-    {
-      invoice: "INV004",
-      paymentStatus: "Paid",
-      totalAmount: "$450.00",
-      paymentMethod: "Credit Card",
-    },
-    {
-      invoice: "INV005",
-      paymentStatus: "Paid",
-      totalAmount: "$550.00",
-      paymentMethod: "PayPal",
-    },
-    {
-      invoice: "INV006",
-      paymentStatus: "Pending",
-      totalAmount: "$200.00",
-      paymentMethod: "Bank Transfer",
-    },
-    {
-      invoice: "INV007",
-      paymentStatus: "Unpaid",
-      totalAmount: "$300.00",
-      paymentMethod: "Credit Card",
-    },
-  ]
-
-  export default function TableDemo() {
+const CustomTableRow: FC = () => {
     return (
-      <Table className="bg">
-        <TableCaption>A list of your recent invoices.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Invoice</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
-            <TableHead className="text-right">Amount</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {invoices.map((invoice) => (
-            <TableRow key={invoice.invoice}>
-              <TableCell className="font-medium">{invoice.invoice}</TableCell>
-              <TableCell>{invoice.paymentStatus}</TableCell>
-              <TableCell>{invoice.paymentMethod}</TableCell>
-              <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+        <>
+            <TableRow className="h-3" />
+            <TableRow>
+                <TableHead className="text-gray-400 h-[42px] w-[100px]">
+                    Course-Code
+                </TableHead>
+                <TableHead className="hidden text-gray-400 sm:table-cell">
+                    Course Title
+                </TableHead>
+                <TableHead className="text-gray-400">Date</TableHead>
+                <TableHead className="text-gray-400">Day</TableHead>
+                <TableHead className="text-gray-400">Shift</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TableCell colSpan={3}>Total</TableCell>
-            <TableCell className="text-right">$2,500.00</TableCell>
-          </TableRow>
-        </TableFooter>
-      </Table>
-    )
-  }
+        </>
+    );
+};
+
+export default function NewTable() {
+    return (
+        <div
+            className="py-14 flex max-[460px]:px-0 px-2 bg-gradient-to-r from-slate-800 
+        to-neutral-950 to-80% text-slate-100"
+        >
+            <Table
+                className="relative justify-center p-1 mx-auto overflow-hidden border border-separate 
+            rounded-lg table-auto max-w-fit sm:max-w-screen-sm lg:max-w-screen-md 
+            border-slate-500"
+            >
+                <TableCaption className="text-white">
+                    IGNOU MCA Dec 2024 datesheet.
+                </TableCaption>
+
+                {/* <TableHeader className="w-full text-center">
+                    <TableRow className="w-full absolute top-5 inset-x-2/4">MCA Dec 2024</TableRow>
+                </TableHeader> */}
+                <TableHeader>
+                    <TableHead className="pb-3 text-base font-bold text-center border-b-4 pt-9 text-sky-300">
+                        SEM I
+                    </TableHead>
+                    <CustomTableRow />
+                </TableHeader>
+                <TableBody>
+                    {courses_1.map((course: Course) => (
+                        <TableRow
+                            key={course.id}
+                            className="hover:bg-gradient-to-r hover:from-orange-500 hover:fro-10% 
+                            hover:via-yellow-500 hover:to-neutral-950 hover:to-90% hover:text-white
+                            active:bg-gradient-to-r active:from-orange-500 active:fro-10% 
+                            active:via-yellow-500 active:to-neutral-950 active:to-90% active:text-white"
+                        >
+                            <TableCell className="font-medium">
+                                {course.course_code}
+                            </TableCell>
+                            <TableCell className="hidden sm:table-cell">
+                                {course.course_name}
+                            </TableCell>
+                            <TableCell>{course.date}</TableCell>
+                            <TableCell>{course.day}</TableCell>
+                            <TableCell className="text-center">
+                                {course.shift}
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+
+                <TableHeader>
+                    <TableHead className="pb-3 text-base font-bold text-center border-b-4 pt-9 text-sky-300">
+                        SEM II
+                    </TableHead>
+                    <CustomTableRow />
+                </TableHeader>
+                <TableBody>
+                    {courses_2.map((course: Course) => (
+                        <TableRow
+                            key={course.id}
+                            className="hover:bg-gradient-to-r hover:from-orange-500 hover:fro-10% 
+                            hover:via-yellow-500 hover:to-neutral-950 hover:to-90% hover:text-white
+                            active:bg-gradient-to-r active:from-orange-500 active:fro-10% 
+                            active:via-yellow-500 active:to-neutral-950 active:to-90% active:text-white"
+                        >
+                            <TableCell className="font-medium">
+                                {course.course_code}
+                            </TableCell>
+                            <TableCell className="hidden sm:table-cell">
+                                {course.course_name}
+                            </TableCell>
+                            <TableCell>{course.date}</TableCell>
+                            <TableCell>{course.day}</TableCell>
+                            <TableCell className="text-center">
+                                {course.shift}
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+
+                <TableHeader>
+                    <TableHead className="pb-3 text-base font-bold text-center border-b-4 pt-9 text-sky-300">
+                        SEM III
+                    </TableHead>
+                    <CustomTableRow />
+                </TableHeader>
+                <TableBody>
+                    {courses_3.map((course: Course) => (
+                        <TableRow
+                            key={course.id}
+                            className="hover:bg-gradient-to-r hover:from-orange-500 hover:fro-10% 
+                            hover:via-yellow-500 hover:to-neutral-950 hover:to-90% hover:text-white
+                            active:bg-gradient-to-r active:from-orange-500 active:fro-10% 
+                            active:via-yellow-500 active:to-neutral-950 active:to-90% active:text-white"
+                        >
+                            <TableCell className="font-medium">
+                                {course.course_code}
+                            </TableCell>
+                            <TableCell className="hidden sm:table-cell">
+                                {course.course_name}
+                            </TableCell>
+                            <TableCell>{course.date}</TableCell>
+                            <TableCell>{course.day}</TableCell>
+                            <TableCell className="text-center">
+                                {course.shift}
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+
+                <TableHeader>
+                    <TableHead className="pb-3 text-base font-bold text-center border-b-4 pt-9 text-sky-300">
+                        SEM IV
+                    </TableHead>
+                    <CustomTableRow />
+                </TableHeader>
+                <TableBody>
+                    {courses_4.map((course: Course) => (
+                        <TableRow
+                            key={course.id}
+                            className="hover:bg-gradient-to-r hover:from-orange-500 hover:fro-10% 
+                            hover:via-yellow-500 hover:to-neutral-950 hover:to-90% hover:text-white
+                            active:bg-gradient-to-r active:from-orange-500 active:fro-10% 
+                            active:via-yellow-500 active:to-neutral-950 active:to-90% active:text-white"
+                        >
+                            <TableCell className="font-medium">
+                                {course.course_code}
+                            </TableCell>
+                            <TableCell className="hidden sm:table-cell">
+                                {course.course_name}
+                            </TableCell>
+                            <TableCell>{course.date}</TableCell>
+                            <TableCell>{course.day}</TableCell>
+                            <TableCell className="text-center">
+                                {course.shift}
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
+    );
+}
